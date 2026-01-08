7d47df7102d96b5b06dc15fbae0faae869d4f59e
inakam <ibuki-nakamura@m3.com>
2026-01-08 23:50:55 +0900

    Add ghqfzf function to enhance repository listing with fzf preview in ghq.zsh

--- dot_zshrc.d/ghq.zsh
+++ dot_zshrc.d/ghq.zsh
@@ -9,3 +9,7 @@ function ghq-fzf() {
 }
 zle -N ghq-fzf
 bindkey '^g' ghq-fzf
+
+function ghqfzf() {
+  ghq list | fzf --preview "ls -laTp $(ghq root)/{} | tail -n+4 | awk '{print \$9\"/\"\$6\"/\"\$7 \" \" \$10}'"
+}
